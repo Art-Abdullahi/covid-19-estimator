7a1bdef4a0a55f994be5c59ee30941069a25b7cd
Abdullahi <abdullahihassanke@gmail.com>
2020-04-09 23:27:18 +0300

    chlg 1 new strategy

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,33 @@
 
 const covid19ImpactEstimator = (data) => {
-  data = input
-  let impact = {
+  const data = input
+  const impact = {
     currentlyAffected: 0,
     infectionsByRequestedTime: 0
   };
-  let severeImpact = {
+  const severeImpact = {
     currentlyAffected: 0,
     infectionsByRequestedTime: 0
   };
   impact.currentlyAffected = data.reportedCases * 10;
   severeImpact.currentlyAffected = data.reportedCases * 50;
-  if (data.periodType == 'days') {
-    impact.infectionsByRequestedTime = impact.currentlyAffected * (2 ** Math.floor(data.timeToElapse / 3))
+  if (data.periodType === 'days') {
+    impact.infectionsByRequestedTime = impact.currentlyAffected
+      * (2 ** Math.floor(data.timeToElapse / 3))
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyAffected * (2 ** Math.floor(data.timeToElapse / 3))
-  }
-  if (data.periodType == 'weeks') {
+  };
+  if (data.periodType === 'weeks') {
     const weeksToDays = data.timeToElapse * 7;
-    impact.infectionsByRequestedTime = impact.currentlyAffected * (2 ** Math.floor(weeksToDays / 3))
+    impact.infectionsByRequestedTime = impact.currentlyAffected
+      * (2 ** Math.floor(weeksToDays / 3))
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyAffected * (2 ** Math.floor(weeksToDays / 3))
-  }
-  if (data.periodType == 'weeks') {
+  };
+  if (data.periodType === 'weeks') {
     const monthToDays = data.timeToElapse * 30;
-    impact.infectionsByRequestedTime = impact.currentlyAffected * (2 ** Math.floor(monthToDays / 3))
+    impact.infectionsByRequestedTime = impact.currentlyAffected
+      * (2 ** Math.floor(monthToDays / 3))
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyAffected * (2 ** Math.floor(monthToDays / 3))
-  }
+  };
 
   return {
     data,
