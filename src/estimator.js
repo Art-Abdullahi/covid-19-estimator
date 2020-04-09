063a0ba35067b1ebfa7e106c174f9f6d361863a1
Abdullahi <abdullahihassanke@gmail.com>
2020-04-09 16:22:40 +0300

    challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,42 @@
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomeInPopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
 const covid19ImpactEstimator = (data) => {
+  const input = data;
+
+  //challenge one
+  let impact = {
+    currentlyAffected: data.reportCases * 10,
+    infectionsByRequestedTime: 0,
+    severeCasesByRequestedTime: 0,
+    hospitalBedsByRequestedTime: 0,
+    casesForICUByRequestedTime: 0,
+    casesForVentilatorsByRequestedTime: 0,
+    dollarsInFlight: 0
+  };
+  let severeImpact = {
+    currentlyAffected: data.reportCases * 50,
+    infectionsByRequestedTime: 0,
+    severeCasesByRequestedTime: 0,
+    hospitalBedsByRequestedTime: 0,
+    casesForICUByRequestedTime: 0,
+    casesForVentilatorsByRequestedTime: 0,
+    dollarsInFlight: 0
+  };
   return {
-    data: {},
-    impact: {},
-    severeImpact: {}
+    data: input,
+    impact,
+    severeImpact
   };
 };
 
