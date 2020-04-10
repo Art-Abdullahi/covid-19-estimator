545a4ff6fd8e8805a7f57c9618f87fff62d2de7c
Abdullahi <abdullahihassanke@gmail.com>
2020-04-11 01:34:59 +0300

    ch 1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,42 +1,64 @@
+
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  const impactCurrentlyAffected = data.reportedCases * 10;
-  const severImpactCurrentlyAffected = data.reportedCases * 50;
-  let impactInRT;
-  let severeImpactInRT;
+  const impactCI = data.reportedCases * 10;
+  const severeImpactCI = data.reportedCases * 50;
+  const days = data.timeToElapse;
+  const aDIP = data.region.avgDailyIncomePopulation;
+  const aDIUSD = data.region.avgDailyIncomeInUSD;
+  let impactIBRT;
+  let severeImpactIBRT;
+  let impactDIF;
+  let severeImpactDIF;
   if (data.periodType === 'days') {
-    impactInRT = impactCurrentlyAffected * (2 ** Math.floor(data.timeToElapse / 3));
-    severeImpactInRT = severImpactCurrentlyAffected * (2 ** Math.floor(data.timeToElapse / 3));
+    impactIBRT = impactCI * (2 ** Math.trunc(days / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.trunc(days / 3));
+    impactDIF = (impactIBRT * aDIP * aDIUSD) / days;
+    severeImpactDIF = (severeImpactIBRT * aDIP * aDIUSD) / days;
   }
   if (data.periodType === 'weeks') {
-    const week2Days = data.timeToElapse * 7;
-    impactInRT = impactCurrentlyAffected * (2 ** Math.floor(week2Days / 3));
-    severeImpactInRT = severImpactCurrentlyAffected * (2 ** Math.floor(week2Days / 3));
+    const weeksToDays = days * 7;
+    impactIBRT = impactCI * (2 ** Math.trunc(weeksToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.trunc(weeksToDays / 3));
+    impactDIF = (impactIBRT * aDIP * aDIUSD) / weeksToDays;
+    severeImpactDIF = (severeImpactIBRT * aDIP * aDIUSD) / weeksToDays;
   }
   if (data.periodType === 'months') {
-    const month2Days = data.timeToElapse * 30;
-    impactInRT = impactCurrentlyAffected * (2 ** Math.floor(month2Days / 3));
-    severeImpactInRT = severImpactCurrentlyAffected * (2 ** Math.floor(month2Days / 3));
+    const monthsToDays = data.timeToElapse * 30;
+    impactIBRT = impactCI * (2 ** Math.trunc(monthsToDays / 3));
+    severeImpactIBRT = severeImpactCI * (2 ** Math.trunc(monthsToDays / 3));
+    impactDIF = (impactIBRT * aDIP * aDIUSD) / monthsToDays;
+    severeImpactDIF = (severeImpactIBRT * aDIP * aDIUSD) / monthsToDays;
   }
+  const impactSCBRT = impactIBRT * 0.15;
+  const severeImpactSCBRT = severeImpactIBRT * 0.15;
+  const impactHBBRT = Math.trunc(data.totalHospitalBeds * 0.35 - impactSCBRT);
+  const severeImpactHBBRT = Math.trunc(data.totalHospitalBeds * 0.35 - severeImpactSCBRT);
+  const impactCFICU = Math.trunc(impactIBRT * 0.05);
+  const severeImpactCFICU = Math.trunc(severeImpactIBRT * 0.05);
+  const impactCFV = Math.trunc(impactIBRT * 0.02);
+  const severeImpactCFV = Math.trunc(severeImpactIBRT * 0.02);
+  impactDIF = Math.trunc(impactDIF);
+  severeImpactDIF = Math.trunc(severeImpactDIF);
   return {
     data: input,
     impact: {
-      currentlyAffected: impactCurrentlyAffected,
-      infectionsByRequestedTime: impactInRT,
-      severeCasesByRequestedTime: 0,
-      hospitalBedsByRequestedTime: 0,
-      casesForICUByRequestedTime: 0,
-      casesForVentilatorsByRequestedTime: 0,
-      dollarsInFlight: 0
+      currentlyInfected: impactCI,
+      infectionsByRequestedTime: impactIBRT,
+      severeCasesByRequestedTime: impactSCBRT,
+      hospitalBedsByRequestedTime: impactHBBRT,
+      casesForICUByRequestedTime: impactCFICU,
+      casesForVentilatorsByRequestedTime: impactCFV,
+      dollarsInFlight: impactDIF
     },
     severeImpact: {
-      currentlyAffected: severImpactCurrentlyAffected,
-      infectionsByRequestedTime: severeImpactInRT,
-      severeCasesByRequestedTime: 0,
-      hospitalBedsByRequestedTime: 0,
-      casesForICUByRequestedTime: 0,
-      casesForVentilatorsByRequestedTime: 0,
-      dollarsInFlight: 0
+      currentlyInfected: severeImpactCI,
+      infectionsByRequestedTime: severeImpactIBRT,
+      severeCasesByRequestedTime: severeImpactSCBRT,
+      hospitalBedsByRequestedTime: severeImpactHBBRT,
+      casesForICUByRequestedTime: severeImpactCFICU,
+      casesForVentilatorsByRequestedTime: severeImpactCFV,
+      dollarsInFlight: severeImpactDIF
     }
   };
 };
